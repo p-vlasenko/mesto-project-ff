fe9d4647aeab7e16a1299d85e326b55771e81997
Pavel Vlasenko <p.s.vlas@yandex.ru>
2025-11-25 01:09:43 +0300

    add button text constants

---
+++ src/workflows/constants.js
@@ -0,0 +1,4 @@
+export const SAVING_TEXT = 'Сохранение...';
+export const DEFAULT_SAVE_BUTTON_TEXT = 'Сохранить';
+export const DEFAULT_CONFIRM_BUTTON_TEXT = 'Да';
+export const DELETION_TEXT = 'Удаление...';
