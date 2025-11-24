89159c2e13b2fe4fd184818448c4f6abb58670a9
Pavel Vlasenko <p.s.vlas@yandex.ru>
2025-11-25 01:59:36 +0300

    fix card adding bug

--- src/workflows/new-card-modal.js
+++ src/workflows/new-card-modal.js
@@ -37,7 +37,7 @@ export const initNewCardModal = ({ resetValidationErrors, addCard, user }) => el
             const newCardElement = makeCardElement(card, user);
             placesListElement.prepend(newCardElement);
             newCardFrom.reset();
-        }).cath(
+        }).catch(
             err => console.error('Card adding failed', err),
         ).finally(() => {
             submitNewCardFormButton.textContent = DEFAULT_SAVE_BUTTON_TEXT;
