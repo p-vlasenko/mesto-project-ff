98e9173e9a1aadabac05c8b93a8e1b5a356cae8f
Pavel Vlasenko <p.s.vlas@yandex.ru>
2025-11-25 01:08:25 +0300

    add avatar modal

---
+++ src/workflows/avatar-modal.js
@@ -0,0 +1,74 @@
+import { initFormModalHandlers } from '../components/modal.js';
+import { DEFAULT_SAVE_BUTTON_TEXT, SAVING_TEXT } from './constants.js';
+
+/**
+ * @typedef {import('../components/modal.js').FromElements} FromElements
+ * @typedef {import('../components/modal.js').ModalElements} ModalElements
+ */
+
+/**
+ * @typedef {object} AvatarElements
+ * @property {HTMLInputElement} avatarImageInput
+ * @property {HTMLImageElement} avatarImageElement
+ */
+
+/**
+ * @typedef {FromElements & ModalElements & AvatarElements} AvatarWorkflowElements
+ */
+
+/** @typedef {import('../types.js').User} User */
+
+/**
+ * @typedef {object} AvatarFlowDeps
+ * @property {User} user
+ * @property {(form: HTMLFormElement) => void} resetValidationErrors
+ * @property {(link: string) => Promise<string>} updateAvatar
+ * @property {(button: HTMLElement) => void} disableButton
+ */
+
+/** @type {(params: ProfileFlowDeps) => (elements: AvatarWorkflowElements) => void} */
+export const initAvatarEditModal = deps => elements => {
+    const { user, updateAvatar, resetValidationErrors, disableButton } = deps;
+
+    const {
+        avatarImageInput,
+        avatarImageElement,
+        form,
+        submitFormButton,
+        ...modalFormElements
+    } = elements;
+
+    /** @type {(url: string) => void} */
+    const setAvatarImage = url => {
+        avatarImageElement.style.backgroundImage = `url('${url}')`;
+        avatarImageInput.value = url;
+    };
+
+    avatarImageInput.value = user.avatar;
+    setAvatarImage(user.avatar);
+
+    /** @type {() => Promise<void>} */
+    const onSubmit = () => {
+        submitFormButton.textContent = SAVING_TEXT;
+
+        return updateAvatar(avatarImageInput.value)
+            .then(setAvatarImage)
+            .finally(() => void (submitFormButton.textContent = DEFAULT_SAVE_BUTTON_TEXT));
+    };
+
+    const setInputValues = () => {
+        avatarImageInput.value = user.avatar;
+    };
+
+    initFormModalHandlers({
+        elements: { form, submitFormButton, ...modalFormElements },
+        handlers: {
+            onSubmit,
+            onOpen: () => {
+                setInputValues();
+                disableButton(submitFormButton);
+            },
+            onClose: () => resetValidationErrors(form),
+        },
+    });
+};
